af5900ced7ab2554d394865092c94fc71a117561
DLong <daveallenlong@gmail.com>
2026-01-15 09:54:27 +0900

    setup: Race page outline

---
+++ src/pages/Race.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { drivers } from "../data/drivers";
+import PredictionRow from "../components/PredictionRow";
+
+export default function Race() {
+	const [prediction, setPrediction] = useState(Array(22).fill(null));
+
+	const updatePrediction = (positionIndex, driverId) => {
+		const next = [...prediction];
+		next[positionIndex] = driverId;
+		setPrediction(next);
+	};
+
+	return (
+		<div className="">
+			<h1 className="">Race Prediction</h1>
+
+			{prediction.map((driverId, index) => (
+				<PredictionRow
+					key={index}
+					position={index + 1}
+					drivers={drivers}
+					selectedDriverId={driverId}
+					onChange={(id) => updatePrediction(index, id)}
+				/>
+			))}
+		</div>
+	);
+}
