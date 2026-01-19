26fd106076a5a5d98e3bff7d38cb9799ce4c97a9
DLong <daveallenlong@gmail.com>
2026-01-19 11:13:04 +0900

    task: save predictions post request

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { drivers } from "./data/drivers.js";
+import { predictions } from "./data/predictions.js";
 
 const app = express();
 
@@ -9,6 +10,21 @@ app.use(express.json());
 
 app.get("/drivers", (req, res) => {
 	res.json(drivers);
+});
+
+app.post("/predictions", (req, res) => {
+	const { raceId, positions } = req.body;
+
+	if (!raceId || !positions) {
+		return res.status(400).json({ error: "Invalid payload" });
+	}
+
+	predictions.push({
+		raceId,
+		positions,
+	});
+
+	res.status(201).json({ message: "Prediction saved" });
 });
 
 const PORT = 3001;
