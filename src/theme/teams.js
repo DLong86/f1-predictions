2f01e3f59ca1812aa7bbb8fed5bac5c5fd65575f
DLong <daveallenlong@gmail.com>
2026-02-03 11:21:19 +0900

    added: teams theme for colour scheme

---
+++ src/theme/teams.js
@@ -0,0 +1,81 @@
+export const teamThemes = {
+	mercedes: {
+		name: "Mercedes",
+		primary: "#00F5D4", // Petronas green
+		glow: "rgba(0, 245, 212, 0.6)",
+		border: "#00F5D4",
+		accent: "#00F5D4",
+	},
+	ferrari: {
+		name: "Ferrari",
+		primary: "#E10600",
+		glow: "rgba(225, 6, 0, 0.6)",
+		border: "#E10600",
+		accent: "#E10600",
+	},
+	mclaren: {
+		name: "McLaren",
+		primary: "#FF8000",
+		glow: "rgba(255, 128, 0, 0.6)",
+		border: "#FF8000",
+		accent: "#FF8000",
+	},
+	redbull: {
+		name: "Red Bull",
+		primary: "#1E41FF",
+		glow: "rgba(30, 65, 255, 0.6)",
+		border: "#1E41FF",
+		accent: "#1E41FF",
+	},
+	williams: {
+		name: "Williams",
+		primary: "#0e45b9",
+		glow: "rgba(20, 104, 219, 0.6)",
+		border: "#0e45b9",
+		accent: "#0e45b9",
+	},
+	// ---------------   Need to fix colour scheme ------------------
+	racingbulls: {
+		name: "Racing Bulls",
+		primary: "#006F62",
+		glow: "rgba(0, 111, 98, 0.6)",
+		border: "#006F62",
+		accent: "#006F62",
+	},
+	// Aston is ok!
+	aston: {
+		name: "Aston Martin",
+		primary: "#006F62",
+		glow: "rgba(0, 111, 98, 0.6)",
+		border: "#006F62",
+		accent: "#006F62",
+	},
+	haas: {
+		name: "Haas",
+		primary: "#006F62",
+		glow: "rgba(0, 111, 98, 0.6)",
+		border: "#006F62",
+		accent: "#006F62",
+	},
+	audi: {
+		name: "Audi",
+		primary: "#006F62",
+		glow: "rgba(0, 111, 98, 0.6)",
+		border: "#006F62",
+		accent: "#006F62",
+	},
+	alpine: {
+		name: "Alpine",
+		primary: "#006F62",
+		glow: "rgba(0, 111, 98, 0.6)",
+		border: "#006F62",
+		accent: "#006F62",
+	},
+	cadillac: {
+		name: "Cadillac",
+		primary: "#006F62",
+		glow: "rgba(0, 111, 98, 0.6)",
+		border: "#006F62",
+		accent: "#006F62",
+	},
+};
